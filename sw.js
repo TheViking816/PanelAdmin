0a88c9583cfb55690a1fbaddda82fbbd1d2e98ab
TheViking816 <adrianlujan94@yahoo.es>
2026-01-06 02:23:13 +0100

    Add PWA assets and default dark mode

---
+++ sw.js
@@ -0,0 +1,52 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('paneladmin-static-v1').then((cache) => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json',
+        './icons/icon-192.png',
+        './icons/icon-512.png',
+        './icons/icon-512-maskable.png',
+        './icons/apple-touch-icon.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== 'paneladmin-static-v1') {
+            return caches.delete(key);
+          }
+          return undefined;
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) {
+        return cached;
+      }
+      return fetch(event.request).then((response) => {
+        const responseClone = response.clone();
+        if (response.status === 200 && response.type === 'basic') {
+          caches.open('paneladmin-static-v1').then((cache) => {
+            cache.put(event.request, responseClone);
+          });
+        }
+        return response;
+      });
+    })
+  );
+});
