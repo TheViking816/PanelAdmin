9d9e0171aeae44b9d645808bf02dcefa6f423c43
TheViking816 <adrianlujan94@yahoo.es>
2026-01-06 01:16:24 +0100

    c

--- vite.config.ts
+++ vite.config.ts
@@ -21,11 +21,3 @@ export default defineConfig(({ mode }) => {
       }
     };
 });
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/Panel-Admin/', // <--- ESTO ES LO MÁS IMPORTANTE
-})
